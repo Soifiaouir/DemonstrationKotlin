910a65b6a9ef93dd15546d7074f8fb10dffce269
Soifiaouir <soifiaouir@gmail.com>
2025-09-16 17:01:08 +0200

    maj notif

---
+++ mod3lazycolumn/src/main/java/com/example/mod3lazycolumn/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.mod3lazycolumn
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mod3lazycolumn.ui.theme.DemonstrationsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DemonstrationsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val articles=List(100){"Article n°$it"}
+                    LazyColumn(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(innerPadding)){
+                        items(articles){
+                            ElevatedCard {
+                        Text("$it", modifier = Modifier.padding(8.dp))
+                            }
+                        }
+                    }
+
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DemonstrationsTheme {
+        Greeting("Android")
+    }
+}
